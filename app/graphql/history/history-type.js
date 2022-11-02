9d71fca1093741343809aabfee3b6aac2014aa28
steve-dickinson <steve.dickinson@defra.gov.uk>
2022-11-02 06:58:57 +0000

    Add additiona; history fields

--- app/graphql/history/history-type.js
+++ app/graphql/history/history-type.js
@@ -4,6 +4,8 @@ const { GraphQLObjectType, GraphQLString } = graphql
 const historyType = new GraphQLObjectType({
   name: 'History',
   fields: () => ({
+    partitionKey: { type: GraphQLString },
+    rowKey: { type: GraphQLString },
     EventType: { type: GraphQLString },
     EventRaised: { type: GraphQLString },
     Payload: { type: GraphQLString },
